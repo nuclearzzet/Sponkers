9d42b9df1a78fdba343cd2c4948467bf1bcbde2f
nuclearzzet <niketjohn@outlook.com>
2022-08-17 17:46:56 +0530

    ✨ Working on command handling

---
+++ src/commands/fun/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder, Message } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName("Ping")
+            .setDescription("Replies with Pong"),
+    
+    async execute(interaction){
+        await interaction.reply("Pong");
+    }
+}   
--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-// imports
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('../config.json');
+const { readdirSync } = require('node:fs');
 
-// creating client
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
-//ready
+client.commands = new Collection();
+
+const commands = readdirSync(`./commands/${dirs}/`).filter(d => d.endsWith('.js'))
+for(let file of commands){
+	let pull = require(`./commands/${dirs}/${file}`);
+	
+}
+
 client.once('ready', () => {
     console.log("Ready!");
 });
 
-//replying to commands
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-    const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-    if(commandName === 'ping'){
-        await interaction.reply('Pong!');
-    };
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-// logging in
 client.login(token);
